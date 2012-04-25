6eca8c6f2c653e1566c5ec469d4941a676f04811
Andrew Lovett-Barron <andrewlb@Nitrogen.local>
2012-04-25 16:01:23 -0400

    Setting up framework

---
+++ app.js
@@ -0,0 +1,44 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = express.createServer();
+
+// Optional since express defaults to CWD/views
+
+// By Matt Sain, on http://stackoverflow.com/questions/4618257/node-js-express-js-how-to-render-less-css
+var less;
+express.compiler.compilers.less.compile = function (str, fn) {
+    if (!less) less = require("less");                                                      
+    try {
+        less.render(str, { compress : true }, fn);
+    } catch (err) {
+        fn(err);
+    }
+};
+
+app.use(express.compiler({ src: publicdir, enable: ['less'] }));
+
+app.configure(function()
+{
+	app.set('views', __dirname + '/project');
+	app.set('view engine', 'jade');
+	app.use(express.bodyDecoder()   );
+	app.use(express.methodOverride());
+	app.use(express.compiler({ src: __dirname + '/bootstrap/less', enable: ['less'] }));
+	app.use(app.router);
+	app.use(express.staticProvider(__dirname + '/public'));
+}
+
+
+app.get('/', function(req, res){
+  res.render('index', { 
+	user: test 
+	
+	});
+});
+
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
