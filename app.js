b7c2694a5040a19f1c9615f9239c382bf8883ad0
Andrew Lovett Barron <andrewlb@Andrew-Lovett-Barrons-MacBook-Air.local>
2012-04-26 17:31:32 -0400

    Small change

--- app.js
+++ app.js
@@ -62,5 +62,5 @@ app.get('/renderpdf', function(req, res){
 
 
 
-app.listen(80);
-//console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
