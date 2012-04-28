ddaf828f4fdf535fbd648cb8909f5b4051e85b98
Andrew Lovett Barron <andrewlb@Andrew-Lovett-Barrons-MacBook-Air.local>
2012-04-28 11:04:08 -0400

    Cleaning up

--- app.js
+++ app.js
@@ -6,31 +6,7 @@ var express = require('express'),
 	fs = require('fs');
 var app = express.createServer();
 
-// Optional since express defaults to CWD/views
-
-// By Matt Sain, on http://stackoverflow.com/questions/4618257/node-js-express-js-how-to-render-less-css
-var less;
-express.compiler.compilers.less.compile = function (str, fn) {
-    if (!less) less = require("less");                                                      
-    try {
-        less.render(str, { compress : true }, fn);
-    } catch (err) {
-        fn(err);
-    }
-};
-
-app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-
-app.configure(function()
-{
-	app.set('views', __dirname + '/project');
-	app.set('view engine', 'jade');
-	app.use(express.bodyParser()   );
-	app.use(express.methodOverride());
-	app.use(express.compiler({ src: __dirname + '/bootstrap/less', enable: ['less'] }));
-	app.use(app.router);
-	app.use(express.static(__dirname + '/public'));
-});
+require('./config.js')(app, express);
 
 
 var test = {
---
+++ app/config.js
@@ -0,0 +1,29 @@
+module.exports = function(app, express){
+
+	// By Matt Sain, on http://stackoverflow.com/questions/4618257/node-js-express-js-how-to-render-less-css
+	var less;
+	express.compiler.compilers.less.compile = function (str, fn) {
+	    if (!less) less = require("less");                                                      
+	    try {
+	        less.render(str, { compress : true }, fn);
+	    } catch (err) {
+	        fn(err);
+	    }
+	};
+
+	app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+
+	app.configure(function()
+	{
+		app.set('views', __dirname + '/project');
+		app.set('view engine', 'jade');
+		app.use(express.bodyParser()   );
+		app.use(express.methodOverride());
+		app.use(express.compiler({ src: __dirname + '/bootstrap/less', enable: ['less'] }));
+		app.use(app.router);
+		app.use(express.static(__dirname + '/public'));
+	});
+
+
+
+}
