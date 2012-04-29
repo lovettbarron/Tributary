d75c1b00c03ae5f0b16b065289edf3f4ab948b07
Andrew Lovett Barron <andrewlb@Andrew-Lovett-Barrons-MacBook-Air.local>
2012-04-29 16:28:27 -0400

    Cleaning up

--- public/js/init.js
+++ public/js/init.js
@@ -1,13 +1,16 @@
 $(document).ready( function() {
+	//Context menu switcher
 	$('ul#context > li').each( function(index) { 
 		$(this).click( function(e) {
-		e.preventDefault();
-		path = $(this).child().attr('id');
-		alert(path);
-		$.get('/' + path, function(data) {
-			$('#content').html(data);
-		});
-		$('#context > li').removeClass('active');
-		$(this).addClass('active');
-	}) }
+			e.preventDefault();
+			path = $(this).child().attr('id');
+			alert(path);
+			$.get('/' + path, function(data) {
+				$('#content').html(data);
+			});
+			$('#context > li').removeClass('active');
+			$(this).addClass('active');
+		}); 
+	})
+
 });
