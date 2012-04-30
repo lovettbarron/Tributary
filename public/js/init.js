0e98d4a4124133da6d6859f0003287409e093f4e
Andrew Lovett-Barron <andrewlb@Nitrogen.local>
2012-04-30 14:42:20 -0400

    Auto lorem via hipsters

--- public/js/init.js
+++ public/js/init.js
@@ -1,16 +1,7 @@
-$(document).ready( function() {
-	//Context menu switcher
-	$('ul#context > li').each( function(index) { 
-		$(this).click( function(e) {
-			e.preventDefault();
-			path = $(this).children('a').attr('id');
-			$.get('/' + path, function(data) {
-					$('#content').html(data);
-				});
-			$('#context > li').removeClass('active');
-			$(this).addClass('active');
-		})
-	});
+$(document).ready(init());
+
+
+function ipsum() {
 	$('.ipsum').each( function(index) {
 			var target = this;
 			$.getJSON('http://hipsterjesus.com/api/', {
@@ -19,5 +10,21 @@ $(document).ready( function() {
 	         $(target).html( data.text );
 	     	});
 		});
+}
 
-});
+function init() {
+		//Context menu switcher
+		$('ul#context > li').each( function(index) { 
+			$(this).click( function(e) {
+				e.preventDefault();
+				path = $(this).children('a').attr('id');
+				$.get('/' + path, function(data) {
+						$('#content').html(data);
+					});
+				$('#context > li').removeClass('active');
+				$(this).addClass('active');
+			})
+		});
+
+		ipsum();
+}
