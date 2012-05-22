3c7057c1d374b1261636664e81cd73010a5d3a47
Matt <matt@emenel.ca>
2012-05-22 17:21:28 -0400

    tweaking for heroku

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/:path', function(req, res){
 // 	//TBD with http://code.google.com/p/wkhtmltopdf/
 // });
 
+var port = process.env.PORT || 3000;
 
-
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
