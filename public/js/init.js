21c52810602bb93f2a39a593ae5d573e64f002c7
Normative <normative@ubuntu-box.(none)>
2012-05-23 11:30:10 -0400

    Fixed SK hover

--- public/js/init.js
+++ public/js/init.js
@@ -11,7 +11,7 @@ $(document).ready( function() {
 
 function init() {
 
-	$('a.brand').hover( function() {
+	$('a.brand').parent().hover( function() {
 			$('.returnHome').show()
 		}, function() {
 			$('.returnHome').hide()
